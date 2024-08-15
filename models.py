4f39847fd1418cf55a84003861fc9ecc0bdbe998
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-08-14 22:26:08 -0700

    Update models.py

--- models.py
+++ models.py
@@ -90,3 +90,139 @@ class UNet(nn.Module):
     final_layer = self.final_layer(decode_block1)
     return  final_layer
 
+
+
+class SpatialTransformation(nn.Module):
+    def __init__(self, use_gpu=False):
+        self.use_gpu = use_gpu
+        super(SpatialTransformation, self).__init__()
+
+    def meshgrid(self, height, width):
+        x_t = torch.matmul(torch.ones([height, 1]), torch.transpose(torch.unsqueeze(torch.linspace(0.0, width -1.0, width), 1), 1, 0))
+        y_t = torch.matmul(torch.unsqueeze(torch.linspace(0.0, height - 1.0, height), 1), torch.ones([1, width]))
+        x_t = x_t.expand([height, width])
+        y_t = y_t.expand([height, width])
+        if self.use_gpu==True:
+            x_t = x_t.cuda()
+            y_t = y_t.cuda()
+        return x_t, y_t
+
+    def repeat(self, x, n_repeats):
+        rep = torch.transpose(torch.unsqueeze(torch.ones(n_repeats), 1), 1, 0)
+        rep = rep.long()
+        x = torch.matmul(torch.reshape(x, (-1, 1)), rep)
+        if self.use_gpu:
+            x = x.cuda()
+        return torch.squeeze(torch.reshape(x, (-1, 1)))
+
+    def interpolate(self, im, x, y):
+        im = F.pad(im, (0,0,1,1,1,1,0,0))
+        batch_size, height, width, channels = im.shape
+        batch_size, out_height, out_width = x.shape
+        x = x.reshape(1, -1)
+        y = y.reshape(1, -1)
+        x = x + 1
+        y = y + 1
+        max_x = width - 1
+        max_y = height - 1
+        x0 = torch.floor(x).long()
+        x1 = x0 + 1
+        y0 = torch.floor(y).long()
+        y1 = y0 + 1
+        x0 = torch.clamp(x0, 0, max_x)
+        x1 = torch.clamp(x1, 0, max_x)
+        y0 = torch.clamp(y0, 0, max_y)
+        y1 = torch.clamp(y1, 0, max_y)
+        dim2 = width
+        dim1 = width*height
+        base = self.repeat(torch.arange(0, batch_size)*dim1, out_height*out_width)
+        base_y0 = base + y0*dim2
+        base_y1 = base + y1*dim2
+        idx_a = base_y0 + x0
+        idx_b = base_y1 + x0
+        idx_c = base_y0 + x1
+        idx_d = base_y1 + x1
+        im_flat = torch.reshape(im, [-1, channels])
+        im_flat = im_flat.float()
+        dim, _ = idx_a.transpose(1,0).shape
+        Ia = torch.gather(im_flat, 0, idx_a.transpose(1,0).expand(dim, channels))
+        Ib = torch.gather(im_flat, 0, idx_b.transpose(1,0).expand(dim, channels))
+        Ic = torch.gather(im_flat, 0, idx_c.transpose(1,0).expand(dim, channels))
+        Id = torch.gather(im_flat, 0, idx_d.transpose(1,0).expand(dim, channels))
+        x1_f = x1.float()
+        y1_f = y1.float()
+        dx = x1_f - x
+        dy = y1_f - y
+        wa = (dx * dy).transpose(1,0)
+        wb = (dx * (1-dy)).transpose(1,0)
+        wc = ((1-dx) * dy).transpose(1,0)
+        wd = ((1-dx) * (1-dy)).transpose(1,0)
+        output = torch.sum(torch.squeeze(torch.stack([wa*Ia, wb*Ib, wc*Ic, wd*Id], dim=1)), 1)
+        output = torch.reshape(output, [-1, out_height, out_width, channels])
+        return output
+
+    def forward(self, moving_image, deformation_matrix):
+        dx = deformation_matrix[:, :, :, 0]
+        dy = deformation_matrix[:, :, :, 1]
+        batch_size, height, width = dx.shape
+        x_mesh, y_mesh = self.meshgrid(height, width)
+        x_mesh = x_mesh.expand([batch_size, height, width])
+        y_mesh = y_mesh.expand([batch_size, height, width])
+        x_new = dx + x_mesh
+        y_new = dy + y_mesh
+        return self.interpolate(moving_image, x_new, y_new)
+
+
+class VoxelMorph2d(nn.Module):
+    def __init__(self, in_channels, use_gpu=False):
+        super(VoxelMorph2d, self).__init__()
+        self.unet = UNet(in_channels, 2)
+        self.spatial_transform = SpatialTransformation(use_gpu)
+        if use_gpu:
+            self.unet = self.unet.cuda()
+            self.spatial_transform = self.spatial_transform.cuda()
+
+    def forward(self, moving_image, fixed_image):
+        x = torch.cat([moving_image, fixed_image], dim=3).permute(0,3,1,2)
+        deformation_matrix = self.unet(x).permute(0,2,3,1)
+        registered_image = self.spatial_transform(moving_image, deformation_matrix)
+        return registered_image  
+
+def cross_correlation_loss(I, J, n):
+    I = I.permute(0, 3, 1, 2)
+    J = J.permute(0, 3, 1, 2)
+    batch_size, channels, xdim, ydim = I.shape
+    I2 = torch.mul(I, I)
+    J2 = torch.mul(J, J)
+    IJ = torch.mul(I, J)
+    sum_filter = torch.ones((1, channels, n, n))
+    if use_gpu:
+        sum_filter = sum_filter.cuda()
+    I_sum = torch.conv2d(I, sum_filter, padding=1, stride=(1,1))
+    J_sum = torch.conv2d(J, sum_filter,  padding=1 ,stride=(1,1))
+    I2_sum = torch.conv2d(I2, sum_filter, padding=1, stride=(1,1))
+    J2_sum = torch.conv2d(J2, sum_filter, padding=1, stride=(1,1))
+    IJ_sum = torch.conv2d(IJ, sum_filter, padding=1, stride=(1,1))
+    win_size = n**2
+    u_I = I_sum / win_size
+    u_J = J_sum / win_size
+    cross = IJ_sum - u_J*I_sum - u_I*J_sum + u_I*u_J*win_size
+    I_var = I2_sum - 2 * u_I * I_sum + u_I*u_I*win_size
+    J_var = J2_sum - 2 * u_J * J_sum + u_J*u_J*win_size
+    cc = cross*cross / (I_var*J_var + np.finfo(float).eps)
+    return torch.mean(cc)
+
+def smooothing_loss(y_pred):
+    dy = torch.abs(y_pred[:, 1:, :, :] - y_pred[:, :-1, :, :])
+    dx = torch.abs(y_pred[:, :, 1:, :] - y_pred[:, :, :-1, :])
+
+    dx = torch.mul(dx, dx)
+    dy = torch.mul(dy, dy)
+    d = torch.mean(dx) + torch.mean(dy)
+    return d/2.0
+
+def vox_morph_loss(y, ytrue, n=9, lamda=0.01):
+    cc = cross_correlation_loss(y, ytrue, n)
+    sm = smooothing_loss(y)
+    loss = -1.0 * cc + lamda * sm
+    return loss
