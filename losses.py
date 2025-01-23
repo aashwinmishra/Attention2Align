a09ef4a90e7d72bed3973bb6c9aedbcdb9ebe1bf
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2025-01-22 23:40:02 -0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -1,3 +1,30 @@
 import torch
 import torch.nn as nn 
 from torch.nn import functional as F
+
+
+def mse_loss(y_pred: torch.tensor, y_target: torch.tensor) -> torch.tensor:
+  """
+  Takes 2 batches of image tensor, of shapes 
+  [batch_size, channels, height, width]. Returns pixel wise mse loss.
+  Args:
+    y_pred: Prediction from spatial transformer network
+    y_target: baseline image batch
+  Returns:
+    tensor value of the loss.
+  """
+  return F.mse_loss(y_pred, y_target)
+
+
+def cc_loss(y_pred: torch.tensor, y_target: torch.tensor) -> torch.tensor:
+  """
+  Takes 2 batches of image tensor, of shapes 
+  [batch_size, channels, height, width]. Returns normalized cross-correlation 
+  between the 2.
+  Args:
+    y_pred: Prediction from spatial transformer network
+    y_target: baseline image batch
+  Returns:
+    tensor value of the loss.
+  """
+  return F.mse_loss(y_pred, y_target)
