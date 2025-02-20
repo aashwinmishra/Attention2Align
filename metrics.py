f34967b4e5d2570ebff603d811fbb5c60b278714
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2025-02-19 22:33:37 -0800

    Create metrics.py

---
+++ metrics.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn 
+from torch.nn import functional as F
+
+
+def TV(img: torch.tensor) -> float:
+  """
+  Computes the Total Variation Metric of a given image,
+  the l_1 norm is used for simplicity.
+  Args:
+    img: input image as a tensor
+  Returns:
+    float value of the metric.
+  """
+  if img.ndim != 2:
+    raise RuntimeError(f"Expected input `img` to be an 2D tensor, but got {img.shape}")
+  diff1 = img[1:, :] - img[:-1, :]
+  diff2 = img[:, 1:] - img[:, :-1]
+  res1 = diff1.abs().sum()
+  res2 = diff2.abs().sum()
+  score = res1 + res2
+  return score
+  
